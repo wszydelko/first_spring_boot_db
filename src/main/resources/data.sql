258ba58397e454498c2fc6f728b644ee5931da1c
Wojciech Szydelko <wszydelko@poczta.onet.pl>
2019-08-01 16:03:26 +0200

    Add "Project 2" to the file data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
 INSERT INTO project(name) VALUES('Projekt 1');
+INSERT INTO project(name) VALUES('Projekt 2');
 
 Insert into task(name, description, budget, done, project_id) values('Task 1', 'Description of task 1', 100.00, 1, SELECT id FROM project WHERE name = 'Projekt 1');
 Insert into task(name, description, budget, done, project_id) values('Task 2', 'Description of task 2 Do', 100.00, 1, SELECT id FROM project WHERE name = 'Projekt 1');
 Insert into task(name, description, budget, done, project_id) values('Task 3', 'Description of task 3 Do', 50.00, 0, SELECT id FROM project WHERE name = 'Projekt 1');
+Insert into task(name, description, budget, done, project_id) values('Task 4', 'Description of task 3 Do', 50.00, 0, select id FROM  project WHERE name = 'Projekt 1');
+
+Insert into task(name, description, budget, done, project_id) values('Task 1', 'Description of task 1', 100.00, 1, SELECT id FROM project WHERE name = 'Projekt 2');
+Insert into task(name, description, budget, done, project_id) values('Task 2', 'Description of task 2 Do', 100.00, 1, SELECT id FROM project WHERE name = 'Projekt 2');
+Insert into task(name, description, budget, done, project_id) values('Task 3', 'Description of task 3 Do', 50.00, 0, SELECT id FROM project WHERE name = 'Projekt 2');
