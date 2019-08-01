797b00d0dd0c32b3ab1c807d1a7ebed886b8ec4e
Wojciech Szydelko <wszydelko@poczta.onet.pl>
2019-08-01 09:23:42 +0200

    Add a SQL data to  the file data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-Insert into task(name, description, budget, done) values('Task 1', 'Description of task 1', 100.00, 1);
-Insert into task(name, description, budget, done) values('Task 2', 'Description of task 2 Do', 100.00, 1);
-Insert into task(name, description, budget, done) values('Task 3', 'Description of task 3 Do', 50.00, 0);
+INSERT INTO project(name) VALUES('Projekt 1');
+
+Insert into task(name, description, budget, done, project_id) values('Task 1', 'Description of task 1', 100.00, 1, SELECT id FROM project WHERE name = 'Projekt 1');
+Insert into task(name, description, budget, done, project_id) values('Task 2', 'Description of task 2 Do', 100.00, 1, SELECT id FROM project WHERE name = 'Projekt 1');
+Insert into task(name, description, budget, done, project_id) values('Task 3', 'Description of task 3 Do', 50.00, 0, SELECT id FROM project WHERE name = 'Projekt 1');
